1ba346f3ee9a8a37be0b23fcfe9b6e5dfbf136ba
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-11-20 03:06:49 +0000

    working on the search and sort features for find.c

---
+++ helpers.c
@@ -0,0 +1,55 @@
+/**
+ * helpers.c
+ *
+ * Helper functions for Problem Set 3.
+ */
+
+#include <cs50.h>
+
+#include "helpers.h"
+
+/**
+ * Returns true if value is in array of n values, else false.
+ */
+bool search(int value, int values[], int n)
+{
+    // TODO: implement a searching algorithm - I choose binary search
+    // while length of list > 0
+    while (n is > 0)
+    {
+        // look at middle of list
+        int min = 0;
+        int max = n-1;
+        int middle = (min + max) / 2; //
+        int target = value;
+
+        for ()
+        {
+            // if the number is found, return true
+            if (n == target)
+            {
+
+            }
+            // else if the number is higher, search left
+            else if (n < middle)
+            {
+
+            }
+            // else if the number is higher, search right
+            else if (n > middle)
+            {
+
+            }
+        }
+    }
+    return false;
+}
+
+/**
+ * Sorts array of n values.
+ */
+void sort(int values[], int n)
+{
+    // TODO: implement a sorting algorithm
+    return;
+}
